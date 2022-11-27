8bf58d4dc5a1543ec1fec322bf2b2ac69d0aae8a
ashlynpark <64673608+ashlynpark@users.noreply.github.com>
2022-11-27 15:25:20 -0800

    Background color

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,12 +1,27 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
+    //const image = { uri: "https://images.app.goo.gl/6fWwLvB9hnuzgihv8"};
+    const image = { uri: "https://storage.googleapis.com/subtlepatterns-production/designers/subtlepatterns/uploads/moroccan-flower-dark.png"};
+
     return (
+        <ImageBackground source={image} style = {styles.container}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text
                 onPress={() => alert('This is the "Home" screen.')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
         </View>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
