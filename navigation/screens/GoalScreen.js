1ae63e038356d1460dff095950f23c465dbd1d33
Mayuri Buckius <mbuckius@uci.edu>
2022-11-27 15:38:06 -0800

    change spacing

--- navigation/screens/GoalScreen.js
+++ navigation/screens/GoalScreen.js
@@ -7,59 +7,82 @@ import GoalItem from '../../components/GoalItem';
 import GoalInput from '../../components/GoalInput'
 
 export default function GoalScreen({ navigation }) {
-   const image = { uri: "https://storage.googleapis.com/subtlepatterns-production/designers/subtlepatterns/uploads/moroccan-flower-dark.png"};
-    const [goals, setGoals] = useState([]);
-    const [myGoals, setMyGoals] = useState([]);
-    const [theirGoals, setTheirGoals] = useState([]);
+  const image = { uri: "https://storage.googleapis.com/subtlepatterns-production/designers/subtlepatterns/uploads/moroccan-flower-dark.png"};
+  const [goals, setGoals] = useState([]);
+  const [myGoals, setMyGoals] = useState([]);
+  const [theirGoals, setTheirGoals] = useState([]);
 
-    const [ismodalVisable, setIsModalVisible] = useState(false);
+  const [ismodalVisable, setIsModalVisible] = useState(false);
 
-    const addUserGoalHandler = (goalTitle, goalDescription, continuous, whoGoalIsFor) => {
-      if (whoGoalIsFor == "me") {
-        setMyGoals([...myGoals,
-          { id: Math.random().toString(), value: goalTitle, description: goalDescription, continuous: continuous}]);
-      }
-      else {
-        setTheirGoals([...theirGoals,
-          { id: Math.random().toString(), value: goalTitle, description: goalDescription, continuous: continuous}]);
-      } 
-      
-      setIsModalVisible(false);
-    };
-
-    const removeMyGoalHandler = goalId => {
-      setMyGoals(goal => {
-          return myGoals.filter((goal) => goal.id !== goalId);
-      });
-    };
-
-    const removeTheirGoalHandler = goalId => {
-      setTheirGoals(goal => {
-          return theirGoals.filter((goal) => goal.id !== goalId);
-      });
-    };
-
-    return (
+  const addUserGoalHandler = (goalTitle, goalDescription, continuous, whoGoalIsFor) => {
+    if (whoGoalIsFor == "me") {
+      setMyGoals([...myGoals,
+        { id: Math.random().toString(), value: goalTitle, description: goalDescription, continuous: continuous}]);
+    }
+    else {
+      setTheirGoals([...theirGoals,
+        { id: Math.random().toString(), value: goalTitle, description: goalDescription, continuous: continuous}]);
+    } 
     
-      <View style={styles.mainContainer}>
-        <ImageBackground source = {image} style = {styles.image}>
+    setIsModalVisible(false);
+  };
+
+  const removeMyGoalHandler = goalId => {
+    setMyGoals(goal => {
+        return myGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const removeTheirGoalHandler = goalId => {
+    setTheirGoals(goal => {
+        return theirGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <ImageBackground source = {image} style = {styles.image}>
+
         <View style = {styles.buttonView}>
             <Button title="Add New Goal" onPress={() => setIsModalVisible(true)} />
         </View>
-      <GoalInput visible={ismodalVisable} onAddGoal={addUserGoalHandler} onCancel={()=>setIsModalVisible(false)} />
-      
-      {
-        (myGoals.length || theirGoals.length) ?
 
-        
-        <View>
-          {
-            myGoals.length ?
+        <GoalInput visible={ismodalVisable} onAddGoal={addUserGoalHandler} onCancel={()=>setIsModalVisible(false)} />
+    
+        {
+          (myGoals.length || theirGoals.length) ?
+
+          
+          <View>
+            {
+              myGoals.length ?
+                <View>
+                  <Text>Your Goals</Text>
+                  <FlatList
+                    style={styles.scrollContainer}
+                    data={myGoals}
+                    keyExtractor={(item, index) => item.id}
+                    renderItem={itemData =>
+                      <GoalItem 
+                        title={itemData.item.value} 
+                        id={itemData.item.id} 
+                        description={itemData.item.description} 
+                        continuous={itemData.item.continuous} 
+                        onDelete={removeMyGoalHandler} 
+                      />}
+                  />
+                </View>
+                :
+                <Text style={styles.emptyMessage}>There are no goals yet for you</Text>
+            }
+            
+            {
+              theirGoals.length ?
               <View>
-                <Text>Your Goals</Text>
+                <Text>Your Partner's Goals</Text>
                 <FlatList
                   style={styles.scrollContainer}
-                  data={myGoals}
+                  data={theirGoals}
                   keyExtractor={(item, index) => item.id}
                   renderItem={itemData =>
                     <GoalItem 
@@ -67,44 +90,21 @@ export default function GoalScreen({ navigation }) {
                       id={itemData.item.id} 
                       description={itemData.item.description} 
                       continuous={itemData.item.continuous} 
-                      onDelete={removeMyGoalHandler} 
+                      onDelete={removeTheirGoalHandler} 
                     />}
                 />
               </View>
               :
-              <Text style={styles.emptyMessage}>There are no goals yet for you</Text>
-          }
-          
-          {
-            theirGoals.length ?
-            <View>
-              <Text>Your Partner's Goals</Text>
-              <FlatList
-                style={styles.scrollContainer}
-                data={theirGoals}
-                keyExtractor={(item, index) => item.id}
-                renderItem={itemData =>
-                  <GoalItem 
-                    title={itemData.item.value} 
-                    id={itemData.item.id} 
-                    description={itemData.item.description} 
-                    continuous={itemData.item.continuous} 
-                    onDelete={removeTheirGoalHandler} 
-                  />}
-              />
-            </View>
-            :
-            <Text style={styles.emptyMessage}>There are no goals yet for your partner</Text>
-          }
-          
-        </View>
-         :
-          <Text style={[styles.emptyMessage, styles.setColorWhite]}>You don't have any goals yet. Sharing experiences together shortens the distance.</Text>
-      }
+              <Text style={styles.emptyMessage}>There are no goals yet for your partner</Text>
+            }
+            
+          </View>
+          :
+            <Text style={[styles.emptyMessage, styles.setColorWhite]}>You don't have any goals yet. Sharing experiences together shortens the distance.</Text>
+        }
       </ImageBackground>
-      </View>
-    
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
